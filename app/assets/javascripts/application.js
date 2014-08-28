87374f4bbb7bd17aa163bc61da4d40467a902be4
Kailey Izard <kizard09@gmail.com>
2014-08-28 10:45:23 -0400

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,45 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+var originalNavClasses;
+
+function toggleNav() {
+    var elem = document.getElementById('custom-menu');
+    var classes = elem.className;
+    if (originalNavClasses === undefined) {
+        originalNavClasses = classes;
+    }
+    elem.className = /expanded/.test(classes) ? originalNavClasses : originalNavClasses + ' expanded';
+}
+
+$(document).ready(function(){
+	$(".custom-menu5").hover(function(){
+		$(".custom-menu5 .link").toggle();
+	});
+	$(".custom-menu4").hover(function(){
+		$(".custom-menu4 .link").toggle();
+	});
+	$(".custom-menu3").hover(function(){
+		$(".custom-menu3 .link").toggle();
+	});
+	$(".custom-menu2").hover(function(){
+		$(".custom-menu2 .link").toggle();
+	});
+	$(".custom-menu1").hover(function(){
+		$(".custom-menu1 .link").toggle();
+	});
+});
