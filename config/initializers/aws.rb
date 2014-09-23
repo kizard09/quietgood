e8a550a86d3232b63065028293887a3da9a55c10
Kailey Izard <kizard09@gmail.com>
2014-09-23 00:00:46 -0400

    removed aws.rb

--- config/initializers/aws.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AKIAJCHAJMQKLJNQDB6A'],
-      :aws_secret_access_key  => ENV['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']
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = ENV['quietgood-assets']
-end
